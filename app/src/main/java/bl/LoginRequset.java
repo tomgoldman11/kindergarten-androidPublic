a899ea14a87678711ae5d4d7de75aa7c10f1ce00
Hajjar <magbar08@campus.haifa.ac.il>
2018-08-26 20:04:35 +0300

    latest updates

---
+++ app/src/main/java/bl/LoginRequset.java
@@ -0,0 +1,19 @@
+package bl;
+
+public class LoginRequset {
+
+    private String username , password;
+
+    public LoginRequset(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}

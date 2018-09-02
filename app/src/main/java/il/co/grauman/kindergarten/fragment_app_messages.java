4e0791d04ae9e9a45b2b13a6f579d55670c7888b
faten_saleh <fatensaleh996@gmail.com>
2018-09-02 14:10:56 +0300

    daily schedule updated

--- app/src/main/java/il/co/grauman/kindergarten/fragment_app_messages.java
+++ app/src/main/java/il/co/grauman/kindergarten/fragment_app_messages.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link fragment_add_event.OnFragmentInteractionListener} interface
+ * {@link fragment_app_messages.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link fragment_add_event#newInstance} factory method to
+ * Use the {@link fragment_app_messages#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_add_event extends Fragment {
+public class fragment_app_messages extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class fragment_add_event extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public fragment_add_event() {
+    public fragment_app_messages() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class fragment_add_event extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_add_event.
+     * @return A new instance of fragment fragment_app_messages.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_add_event newInstance(String param1, String param2) {
-        fragment_add_event fragment = new fragment_add_event();
+    public static fragment_app_messages newInstance(String param1, String param2) {
+        fragment_app_messages fragment = new fragment_app_messages();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class fragment_add_event extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_add_event, container, false);
+        return inflater.inflate(R.layout.fragment_app_messages, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event

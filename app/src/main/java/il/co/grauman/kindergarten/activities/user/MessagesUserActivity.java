308c030f06b0cb80bccf1cdab9795ea129ca8d90
Ahmed Yusef <ahmedaboalhija9@gmail.com>
2018-09-03 15:28:34 +0300

    Messages-user-activity

---
+++ app/src/main/java/il/co/grauman/kindergarten/activities/user/MessagesUserActivity.java
@@ -0,0 +1,15 @@
+package il.co.grauman.kindergarten.activities.user;
+
+import android.os.Bundle;
+
+import il.co.grauman.kindergarten.R;
+import il.co.grauman.kindergarten.activities.common.BaseDrawerActivity;
+
+public class MessagesUserActivity extends BaseDrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.a);
+    }
+}

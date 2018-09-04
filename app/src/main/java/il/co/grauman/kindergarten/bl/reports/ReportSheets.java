3333e58b692ae88981a294170057570ac68d74c7
Hajjar <magbar08@campus.haifa.ac.il>
2018-09-04 13:41:49 +0300

    static

--- app/src/main/java/il/co/grauman/kindergarten/bl/reports/ReportSheets.java
+++ app/src/main/java/il/co/grauman/kindergarten/bl/reports/ReportSheets.java
@@ -15,47 +15,58 @@ import il.co.grauman.kindergarten.bl.reports.reportsRequests.EmployeeReportsRequ
 import il.co.grauman.kindergarten.bl.reports.reportsRequests.ReportsRequest;
 import retrofit2.Callback;
 
-public class ReportSheets implements ReportSheetsApi {
-    @Override
-    public void getHoursReportForWorker(int month , int year, String userID , retrofit2.Callback<List<WorkHours>> callback){
-        ApiImplementation.apiImplementation(callback , ()-> RetrofitInstance.getInstance()
-        .getApi().getHoursReportForWorker(new EmployeeReportsRequest(month , year , userID)));
-    }
+public class ReportSheets {
+    private static ReportSheetsApi instance;
 
-    @Override
-    public void getHoursReport(int month, int year, Callback<List<WorkHours>> callback) {
-        ApiImplementation.apiImplementation(callback , ()-> RetrofitInstance.getInstance()
-        .getApi().getHoursReport(new ReportsRequest(month, year)));
-    }
+    public static ReportSheetsApi getInstace() {
+        if(instance == null) {
+            instance = new ReportSheetsApi() {
 
-    @Override
-    public void getDailySchedule(Date day, Callback<List<DayActivtey>> callback) {
-        ApiImplementation.apiImplementation(callback , ()-> RetrofitInstance.getInstance()
-        .getApi().getDailySchedule(day));
-    }
+                @Override
+                public void getHoursReportForWorker(int month, int year, String
+                        userID, retrofit2.Callback<List<WorkHours>> callback) {
+                    ApiImplementation.apiImplementation(callback, () -> RetrofitInstance.getInstance()
+                            .getApi().getHoursReportForWorker(new EmployeeReportsRequest(month, year, userID)));
+                }
 
-    @Override
-    public void getDailySum(Date day, Callback<DailySummery> callback) {
-        ApiImplementation.apiImplementation(callback , ()-> RetrofitInstance.getInstance()
-        .getApi().getDailySum(day));
-    }
+                @Override
+                public void getHoursReport(int month, int year, Callback<List<WorkHours>> callback) {
+                    ApiImplementation.apiImplementation(callback, () -> RetrofitInstance.getInstance()
+                            .getApi().getHoursReport(new ReportsRequest(month, year)));
+                }
 
-    @Override
-    public void getLateChildren(Date day, Callback<List<Child>>callback){
-        ApiImplementation.apiImplementation(callback , ()-> RetrofitInstance.getInstance()
-        .getApi().getLateChildren(day));
-    }
+                @Override
+                public void getDailySchedule(Date day, Callback<List<DayActivtey>> callback) {
+                    ApiImplementation.apiImplementation(callback, () -> RetrofitInstance.getInstance()
+                            .getApi().getDailySchedule(day));
+                }
 
-    @Override
-    public void reportsTo(Report report , retrofit2.Callback<StatusResponse> callback){
-        ApiImplementation.apiImplementation(callback , ()-> RetrofitInstance.getInstance()
-        .getApi().reportsTo(report));
-    }
+                @Override
+                public void getDailySum(Date day, Callback<DailySummery> callback) {
+                    ApiImplementation.apiImplementation(callback, () -> RetrofitInstance.getInstance()
+                            .getApi().getDailySum(day));
+                }
 
-    @Override
-    public void getReports(retrofit2.Callback<List<Report>> callback){
-        ApiImplementation.apiImplementation(callback , ()-> RetrofitInstance.getInstance()
-        .getApi().getReports());
+                @Override
+                public void getLateChildren(Date day, Callback<List<Child>> callback) {
+                    ApiImplementation.apiImplementation(callback, () -> RetrofitInstance.getInstance()
+                            .getApi().getLateChildren(day));
+                }
+
+                @Override
+                public void reportsTo(Report report, retrofit2.Callback<StatusResponse> callback) {
+                    ApiImplementation.apiImplementation(callback, () -> RetrofitInstance.getInstance()
+                            .getApi().reportsTo(report));
+                }
+
+                @Override
+                public void getReports(retrofit2.Callback<List<Report>> callback) {
+                    ApiImplementation.apiImplementation(callback, () -> RetrofitInstance.getInstance()
+                            .getApi().getReports());
+                }
+            };
+        }
+        return instance;
     }
 
 

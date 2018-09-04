1cfce789add0ca6f054bdf91369fceb5d98aa301
Fawzi Abo Shkara <Fozi.1998@gmail.com>
2018-09-04 15:41:40 +0300

    - Added activity for the add event to calendar. - Added string resouces. * Need to set onClickListener and navigate to the new Activity with startActivityFroResult.

---
+++ app/src/main/java/il/co/grauman/kindergarten/activities/admin/AddEventToCalendar.java
@@ -0,0 +1,12 @@
+package il.co.grauman.kindergarten.activities.admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddEventToCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
